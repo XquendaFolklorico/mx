d820557be0c161a05fe94ce1208807d54e837150
JesusMoralesF <emorales41@gmail.com>
2018-08-26 15:19:13 -0500

    menu

--- homeapp.js
+++ homeapp.js
@@ -1,16 +1,14 @@
-/*
-$('.menu-toggle').click(function() {
-
-  $('.site-nav').toggleClass('site-nav--open');
-  $(this).toggleClass('open');
-  
-})
-*/
-
 var menuToggle= document.querySelector('.menu-toggle')
 
-menuToggle.addEventListener("click",function(){
+function menu(){
   var siteNav= document.querySelector('.site-nav')
   siteNav.classList.toggle('site-nav--open')
   menuToggle.classList.toggle('open')
-})
+}
+
+menuToggle.addEventListener("click",menu)
+document.querySelector('.fa-info').parentElement.addEventListener("click",menu)
+document.querySelector('.fa-images').parentElement.addEventListener("click",menu)
+document.querySelector('.fa-calendar-alt').parentElement.addEventListener("click",menu)
+document.querySelector('.fa-hand-point-right').parentElement.addEventListener("click",menu)
+document.querySelector('.fa-envelope').parentElement.addEventListener("click",menu)
