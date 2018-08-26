38b610a4a5a565e613841690aec1282ce57a1623
JesusMoralesF <emorales41@gmail.com>
2018-08-26 09:19:23 -0500

    pure js

--- homeapp.js
+++ homeapp.js
@@ -1,6 +1,16 @@
+/*
 $('.menu-toggle').click(function() {
+
+  $('.site-nav').toggleClass('site-nav--open');
+  $(this).toggleClass('open');
   
-    $('.site-nav').toggleClass('site-nav--open', 500);
-    $(this).toggleClass('open');
-    
-  })
+})
+*/
+
+var menuToggle= document.querySelector('.menu-toggle')
+
+menuToggle.addEventListener("click",function(){
+  var siteNav= document.querySelector('.site-nav')
+  siteNav.classList.toggle('site-nav--open')
+  menuToggle.classList.toggle('open')
+})
