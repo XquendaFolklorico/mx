7a9d1b36ccd024fd927db7620a5c7dcfe5c12c24
JesusMoralesF <emorales41@gmail.com>
2018-08-26 21:31:13 -0500

    scroll

--- homeapp.js
+++ homeapp.js
@@ -1,9 +1,11 @@
 var menuToggle= document.querySelector('.menu-toggle')
 
-function menu(){
+function menu(e){
+  
   var siteNav= document.querySelector('.site-nav')
   siteNav.classList.toggle('site-nav--open')
   menuToggle.classList.toggle('open')
+ 
 }
 
 menuToggle.addEventListener("click",menu)
@@ -12,3 +14,22 @@ document.querySelector('.fa-images').parentElement.addEventListener("click",menu
 document.querySelector('.fa-calendar-alt').parentElement.addEventListener("click",menu)
 document.querySelector('.fa-hand-point-right').parentElement.addEventListener("click",menu)
 document.querySelector('.fa-envelope').parentElement.addEventListener("click",menu)
+
+/*
+if(screen.width<770){
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function(e) {
+    var currentScrollPos = window.pageYOffset;
+
+    if (prevScrollpos < currentScrollPos) {
+      document.querySelector("#barra").style.display = "none";
+    } else {
+      document.querySelector("#barra").style.display = "block";
+      
+    }
+
+    prevScrollpos = currentScrollPos;
+  }
+}
+
+*/
